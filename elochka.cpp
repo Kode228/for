f5ab0b192eeb2de9f75224e00e489da49aea82fe
Kode228 <fpam00873@gmail.com>
2024-10-15 15:32:52 +0900

    elochka *

---
+++ elochka.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int height;
+    setlocale(LC_ALL, "Russian");
+
+
+    cout << "Введите высоту елки: ";
+    cin >> height;
+
+    // Верхняя часть елки
+    for (int i = 1; i <= height; ++i) {
+        // Пробелы для создания отступа
+        for (int j = 1; j <= height - i; ++j) {
+            cout << " ";
+        }
+        // Звездочки для елки
+        for (int j = 1; j <= 2 * i - 1; ++j) {
+            cout << "*";
+        }
+        cout << endl; // Переход на новую строку
+    }
+
+   
+
+    return 0;
+}
